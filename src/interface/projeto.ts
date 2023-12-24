96565f629bc382ba87a6f9959048c7f902cdc285
Fernando Henrique da Silva <feroficialvolei@gmail.com>
2023-12-23 23:02:43 -0300

    criando o modelo do login e projeto

---
+++ src/interface/projeto.ts
@@ -0,0 +1,34 @@
+
+export interface Projeto {
+    id: string,
+    nome: string,
+    prazo: string,
+    descricao: string,
+    ColaboradorProjeto?: [
+        {
+            colaborador_id: number,
+            projeto_id: number,
+            id_colaborador: {
+                id: number,
+                nome: string,
+                email: string,
+                senha: string,
+                idade: string,
+                role: string,
+                regime_contratacao: string
+            }
+        }
+    ],
+    projetoTecnologias?: [
+        {
+            tecnologia_id: number,
+            projeto_id: number,
+            id_tecnologia: {
+                id: number,
+                nome_tecnologia: string
+            }
+        }
+    ]
+}
+
+
